c1ace7b472f2bb4d860a782431c9fcbd33dee42c
Adam Jankowski <ghost17814@gmail.com>
2023-04-18 00:27:37 -0400

    yesir

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -9,11 +10,13 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
-
+            var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+            var evens = new List<int>();
+            var odds = new List<int>();
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -21,12 +24,43 @@ namespace ArraysAndLists
              * Then add those numbers to either the evens List
              * or the odds List
              */
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+                if (numbers[i] % 2 == 0) 
+                {
+                    evens.Add(numbers[i]);
+                }
+                else 
+                {
+                    odds.Add(numbers[i]);
+                }
+            }
 
+
+            //foreach (var num in numbers) 
+            //{
+            //    if (num % 2 == 0) 
+            //    {
+            //        evens.Add(num);
+            //    }
+            //    else 
+            //    {
+            //        odds.Add(num);
+            //    }
+            //}
             /* Now using foreach or for loops,
              * display each List of even and odd numbers
              *
              * Try to be creative in your display
              */
+            foreach (var evenNum in evens)
+            {
+                Console.WriteLine($"Are you really asking me if {evenNum} is even or odd? Even, duh!");
+            }
+            foreach (var oddNum in odds)
+            {
+                Console.WriteLine($"Preeeeeety sure {oddNum} is odd but don't quote me...");
+            }
         }
     }
 }
